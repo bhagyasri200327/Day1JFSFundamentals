73f6616ab80ea7ee4324e22bfc3a4debb20463ca
Surisetti Bhagya Sri <bhagya@example.com>
2025-08-29 20:47:08 +0530

    jfs41java

--- IncomeTax.java
+++ IncomeTax.java
@@ -5,7 +5,9 @@ public class IncomeTax {
 	public static void main(String[] args) {
 		Scanner sc= new Scanner(System.in);
 		double income= sc.nextInt();
-		double taxrate = income<30000 ? 0.05*income : (income>30000 && income <100000)?income*0.1: (income<200000)? income*0.15: income*0.14;
+		double taxrate = income<300000 ? 0.05*income 
+				: (income>30000 && income <1000000)?income*0.1
+						:(income<2000000)? income*0.15: income*0.14;
 		System.out.println(taxrate);
 		}
 	
